e2ffd6c475b9cabfb4a82d533109727193fc7439
Ignacio Segura <37955612+Ta11ega1@users.noreply.github.com>
2019-04-23 10:25:14 -0400

    Descarga excel
    Descarga excel del lado del cliente, pero debe estar el excel en el servidor :/

--- detalles.py
+++ detalles.py
@@ -1,28 +1,43 @@
 # -*- coding: utf-8 -*-
 import xlsxwriter
+import base64, cStringIO
+import xlwt
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
-import base64, cStringIO
+import os, sys, stat
 
+from openerp.http import request
+from openerp import http
+from openerp.addons.web.controllers.main import serialize_exception,content_disposition
+
+
+class Binary(http.Controller):
+    @http.route('/web/binary/download_report', type='http', auth="public")
+    @serialize_exception
+    def download_xls_document(self, path, filename, **kw):
+        with open(path, "rb") as pdf_file:
+            return request.make_response(pdf_file.read(),
+                                     [('Content-Type', 'application/octet-stream'),
+                                      ('Content-Disposition', content_disposition(filename))])
+									  
 class detalles(models.TransientModel):
  	_name = 'mi_modulo.mi_tabla_'
 	
+	file = fields.Binary()
+	filename = fields.Char()
 	
 	@api.multi
-	def invoice_confirm_dte(self, context=None):
-		output = cStringIO.StringIO()
-		self.env.cr.execute('select name, description, selection from mi_modulo_mi_tabla')
-		datos = self.env.cr.fetchall()
-		workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-		#workbook = xlsxwriter.Workbook('hola.xlsx')
+	def imprimir_excel(self, context=None):
+		data2 = cStringIO.StringIO()
+		#workbook = xlsxwriter.Workbook(data2, {'in_memory': True})
+		workbook = xlsxwriter.Workbook('nuevohello.xlsx')
 		worksheet = workbook.add_worksheet()
 		if context is None:
 			context = {}
 		active_ids = context.get('active_ids', []) or []
 		registros = self.env['mi_modulo.mi_tabla'].browse(active_ids)
-		#cells = xlsxwriter.utility.xl_range(0,0, 1, 1)
-		#worksheet.add_table(cells, {'data': 'hola', 'total_row', 1, 'columns': 1})
+		
 		worksheet.write('A1', 'Nombre')
 		worksheet.write('B1', 'Descripcion')
 		worksheet.write('C1', 'Seleccion')
@@ -37,25 +52,26 @@ class detalles(models.TransientModel):
 		
 
 		workbook.close()
-		output.seek(0)
-		vals = {
-			'name': 'something',
-			'datas_fname': 'name.xlsx',
-			'description': 'XXXXXXXXXXXXX',
-			'parent_id': 'mi_modulo_4',
-			'type': 'binary',
-			'db_datas': base64.encodestring(output.read()),
-        }        
-        file_id = self.pool.get('ir.attachment').create(vals,context=context)
+		return {
+			'type': 'ir.actions.act_url',
+			'url': '/web/binary/download_report?path=%s&filename=%s' % ('C:/Program Files (x86)/Odoo 10.0/nuevohello.xlsx', 'nuevohello.xlsx'),
+			'target': 'blank',
+		}
+	
+	@api.multi
+	def invoice_confirm_dte(self):
+		path = os.chmod('C:\Users\Systcom\Downloads', 0666)
+		
 		"""return {
 			'view_type': 'form',
 			'view_mode': 'form',
 			'res_model': 'mi_modulo.mi_tabla_',
 			'type': 'ir.actions.act_window',
 			'target': 'new',
-			'context': {'default_file':base64.encodestring(output.read()), 'default_filename':'hola.xlsx'}
-		
+			'context': {'default_file':self.file, 'default_filename':self.filename},
 		}"""
 		
-		
-       
+
+	def eliminar_excel(self):
+		os.remove('nuevohello.xlsx')
+	
